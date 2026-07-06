8e4cda04454e6401f310d31fc83cbe47662a576c
Kentaro Azuma <azuma.199x@gmail.com>
2026-07-06 15:03:03 +0900

    Add関数の実装
    このコミットはとりあえずCopilotでできるところまで作らせてみた段階の内容となっている。
    そのため、ビルドなどはまだ通らない可能性が高い。

---
+++ four_operations/include/four_operations.h
@@ -0,0 +1,27 @@
+/**
+ * @file four_operations.h
+ * @brief 四則演算を行うための関数を宣言するヘッダファイル。
+ *
+ * このファイルには、加算などの基本的な四則演算処理を宣言する。
+ */
+
+#ifndef FOUR_OPERATIONS_H_INCLUDED
+#define FOUR_OPERATIONS_H_INCLUDED
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+#include <stdint.h>
+
+/**
+ * @brief 2つの整数を加算する。
+ *
+ */
+int8_t add(int8_t a, int8_t b);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* FOUR_OPERATIONS_H_INCLUDED */
